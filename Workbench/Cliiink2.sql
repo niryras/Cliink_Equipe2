8ba726e8fda0a23ddc3f2da048c73e33d85664cb
LucaPelliccioli <luca.pelliccioli.pro@gmail.com>
2020-11-25 14:46:34 +0100

    edit

--- Workbench/Cliiink2.sql
+++ Workbench/Cliiink2.sql
@@ -7,12 +7,11 @@ USE `CLIIINK2`;
 CREATE TABLE IF NOT EXISTS `entreprise` (
   `en_id` int NOT NULL AUTO_INCREMENT,
   `en_nom` varchar(50) NOT NULL,
-  `en_siren` varchar(50) NOT NULL,
+  `en_siren` int NOT NULL,
   `en_secteur_activité` varchar(200) NOT NULL,
   `en_adresse` varchar(5) NOT NULL,
   `en_code_postal` varchar(50) NOT NULL,
   `en_ville` varchar(10) NOT NULL,
-  `en_géolocalisation` varchar(50) NOT NULL,
   `en_vi_id_fk` int NOT NULL,
   `en_sa_id_fk` int NOT NULL,
   KEY `ID` (`en_id`)
@@ -33,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `secteur_activité` (
 CREATE TABLE IF NOT EXISTS `population` (
   `po_id` int NOT NULL AUTO_INCREMENT,
   `po_source` varchar(50) NOT NULL,
-  `po_année` varchar(50) NOT NULL,
+  `po_année` int NOT NULL,
   `po_statut_pro` varchar(50) NOT NULL,
   `po_CSP` varchar(50) NOT NULL,
   `po_nbre_pop` varchar(50) NOT NULL,
