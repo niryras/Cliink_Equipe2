b10b650ef1ff8095096d177f4f1086dc23999d31
LucaPelliccioli <luca.pelliccioli.pro@gmail.com>
2020-11-28 16:56:29 +0100

    edit

--- Workbench/Cliiink2.sql
+++ Workbench/Cliiink2.sql
@@ -4,14 +4,16 @@ USE `CLIIINK2`;
 -- ----------------------------
 -- Table structure for entreprise
 -- ----------------------------
+
 CREATE TABLE IF NOT EXISTS `entreprise` (
   `en_id` int NOT NULL AUTO_INCREMENT,
-  `en_nom` varchar(50) NOT NULL,
   `en_siren` int NOT NULL,
-  `en_secteur_activité` varchar(200) NOT NULL,
-  `en_adresse` varchar(5) NOT NULL,
-  `en_code_postal` varchar(50) NOT NULL,
-  `en_ville` varchar(10) NOT NULL,
+  `en_nic` int NOT NULL,
+  `en_dénomination_unité_légal` varchar(50) NOT NULL,
+  `en_activité_principale_établissement` varchar(50) NOT NULL,
+  `en_adresse` varchar(50) NOT NULL,
+  `en_codePostal` int(5) NOT NULL,
+  `en_ville` varchar(50) NOT NULL,
   `en_vi_id_fk` int NOT NULL,
   `en_sa_id_fk` int NOT NULL,
   KEY `ID` (`en_id`)
@@ -29,13 +31,14 @@ CREATE TABLE IF NOT EXISTS `secteur_activité` (
 -- ----------------------------
 -- Table structure for population
 -- ----------------------------
+
 CREATE TABLE IF NOT EXISTS `population` (
   `po_id` int NOT NULL AUTO_INCREMENT,
   `po_source` varchar(50) NOT NULL,
-  `po_année` int NOT NULL,
+  `po_année` date NOT NULL,
   `po_statut_pro` varchar(50) NOT NULL,
   `po_CSP` varchar(50) NOT NULL,
-  `po_nbre_pop` varchar(50) NOT NULL,
+  `po_nbre_pop` int NOT NULL,
   `po_se_id_fk` int NOT NULL,
   `po_ag_id_fk` int NOT NULL,
   `po_vi_id_fk` int NOT NULL,
@@ -79,7 +82,7 @@ CREATE TABLE IF NOT EXISTS `age` (
 CREATE TABLE IF NOT EXISTS `logement` (
   `lo_id` int NOT NULL AUTO_INCREMENT,
   `lo_nom` varchar(50) NOT NULL,
-  `lo_nbre` varchar(50) NOT NULL,
-  `lo_vi_id_fk` varchar(50) NOT NULL,
+  `lo_nbre` int NOT NULL,
+  `lo_vi_id_fk` int NOT NULL,
   KEY `ID` (`lo_id`)
 );
