00d978dec9039094c26678e853daa1069b50f77a
LucaPelliccioli <luca.pelliccioli.pro@gmail.com>
2020-11-30 10:35:50 +0100

    edit

---
+++ Workbench/Cliink2 Commandes BDD.sql
@@ -0,0 +1,99 @@
+CREATE DATABASE  IF NOT EXISTS `CLIIINK2` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `CLIIINK2`;
+
+-- ----------------------------
+-- Table structure for entreprise
+-- ----------------------------
+
+CREATE TABLE IF NOT EXISTS `entreprise` (
+  `en_id` int NOT NULL AUTO_INCREMENT,
+  `en_siren` int NOT NULL,
+  `en_nic` int NOT NULL,
+  `en_dénomination_unité_légal` varchar(50) NOT NULL,
+  `en_activité_principale_établissement` varchar(50) NOT NULL,
+  `en_adresse` varchar(50) NOT NULL,
+  `en_codePostal` int(5) NOT NULL,
+  `en_ville` varchar(50) NOT NULL,
+  `en_vi_id_fk` int NOT NULL,
+  `en_sa_id_fk` int NOT NULL,
+  KEY `ID` (`en_id`)
+);
+
+-- ----------------------------
+-- Table structure for secteur_activité
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `secteur_activité` (
+  `sa_id` int NOT NULL AUTO_INCREMENT,
+  `sa_nom` varchar(50) NOT NULL,
+  KEY `ID` (`sa_id`)
+);
+
+-- ----------------------------
+-- Table structure for population
+-- ----------------------------
+
+CREATE TABLE IF NOT EXISTS `population` (
+  `po_id` int NOT NULL AUTO_INCREMENT,
+  `po_source` varchar(50) NOT NULL,
+  `po_année` date NOT NULL,
+  `po_nbre_pop` int NOT NULL,
+  `po_csp_id_fk` int NOT NULL,
+  `po_ag_id_fk` int NOT NULL,
+  `po_vi_id_fk` int NOT NULL,
+  KEY `ID` (`po_id`)
+);
+
+-- ----------------------------
+-- Table structure for ville
+-- ----------------------------
+
+CREATE TABLE IF NOT EXISTS `ville` (
+  `vi_id` int NOT NULL AUTO_INCREMENT,
+  `vi_nom` varchar(50) NOT NULL,
+  KEY `ID` (`vi_id`)
+);
+
+-- ----------------------------
+-- Table structure for catégorie_socio_pro
+-- ----------------------------
+
+CREATE TABLE IF NOT EXISTS `catégorie_socio_pro` (
+  `csp_id` int NOT NULL AUTO_INCREMENT,
+  `csp_nom` varchar(50) NOT NULL,
+  KEY `ID` (`csp_id`)
+);
+
+-- ----------------------------
+-- Table structure for age
+-- ----------------------------
+
+CREATE TABLE IF NOT EXISTS `age` (
+  `ag_id` int NOT NULL AUTO_INCREMENT,
+  `ag_tranche_age` varchar(50) NOT NULL,
+  KEY `ID` (`ag_id`)
+);
+
+-- ----------------------------
+-- Table structure for logement
+-- ----------------------------
+
+CREATE TABLE IF NOT EXISTS `logement` (
+  `lo_id` int NOT NULL AUTO_INCREMENT,
+  `lo_nbre` varchar(50) NOT NULL,
+  `lo_vi_id_fk` int NOT NULL,
+  `lo_cl_id_fk` int NOT NULL,
+  KEY `ID` (`lo_id`)
+);
+
+-- ----------------------------
+-- Table structure for catégorie_logement
+-- ----------------------------
+
+CREATE TABLE IF NOT EXISTS `catégorie_logement` (
+  `cl_id` int NOT NULL AUTO_INCREMENT,
+  `cl_nom` int NOT NULL,
+  KEY `ID` (`cl_id`)
+);
+
+
+
